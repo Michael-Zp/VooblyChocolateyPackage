01117bcbe7f32fe4a5074d5bf23748f22bc765b0
MichaelZp <michael.zappe@yahoo.de>
2019-08-20 22:17:25 +0200

    Changed fileType in uninstall to EXE

--- Voobly/tools/chocolateyuninstall.ps1
+++ Voobly/tools/chocolateyuninstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'Voobly*'
-  fileType      = 'EXE_MSI_OR_MSU'
+  fileType      = 'EXE'
   silentArgs    = "/qn /norestart"
   validExitCodes= @(0, 3010, 1605, 1614, 1641)
 }
